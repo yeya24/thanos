1b6abfcb35e2a7153a9dabda921926a7f72b8c7f
yeya24 <yb532204897@gmail.com>
2021-02-27 01:29:52 -0500

    fix exemplars implementation
    Signed-off-by: yeya24 <yb532204897@gmail.com>

--- pkg/exemplars/exemplars_test.go
+++ pkg/exemplars/exemplars_test.go
@@ -4,7 +4,9 @@
 package exemplars
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/thanos-io/thanos/pkg/exemplars/exemplarspb"
 	"github.com/thanos-io/thanos/pkg/store/labelpb"
@@ -13,6 +15,12 @@ import (
 
 func TestMain(m *testing.M) {
 	testutil.TolerantVerifyLeakMain(m)
+}
+
+func TestA(t *testing.T) {
+	tt, err := time.Parse(time.RFC3339, "1614395157.954")
+	fmt.Printf("%v\n", err)
+	fmt.Printf("%v\n", tt)
 }
 
 func TestDedupExemplarsData(t *testing.T) {
